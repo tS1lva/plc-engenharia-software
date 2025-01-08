c5facd0e78f7d58810323b931b050c26d76223cc
Tiago Silva <tiago.ps2@puccampinas.edu.br>
2025-01-08 11:39:46 -0300

    primeiros passos dia 20, ajustando `status.js`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,13 @@
 import database from "infra/database.js"
 
 async function status (request, response) {
-  const result = await database.query('SELECT 1+11 as sum;');
-  console.log(result.rows);
-  response.status(200).json("mensagem default API");
+  const updatedAt = new Date().toISOString();
+  console.log();
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    postgres_version: "nonee"
+  });
 }
 
 export default status;
