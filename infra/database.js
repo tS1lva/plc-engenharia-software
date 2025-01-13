5d340f128c3681877a233333d6252e55d808582b
Tiago Silva <tiago.ps2@puccampinas.edu.br>
2025-01-13 10:45:27 -0300

    makes `database.js` and `client.connect()` more manager

--- infra/database.js
+++ infra/database.js
@@ -9,6 +9,14 @@ async function query(queryObject) {
     password: process.env.POSTGRES_PASSWORD
   }
 
+  console.log('Credenciais do Postgres: ', {
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD
+  });
+
   const client = new Client(clientData);
 
 
@@ -18,6 +26,7 @@ async function query(queryObject) {
     return result;
   } catch (error) {
     console.log(error);
+    throw error;
   } finally {
     await client.end();
   }
