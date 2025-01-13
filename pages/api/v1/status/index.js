7cc2e663b90949f3dfc5cc38825859378317ad5c
Tiago Silva <tiago.ps2@puccampinas.edu.br>
2025-01-13 10:03:05 -0300

    finaliza o endpoint de status

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -10,10 +10,12 @@ async function status (request, response) {
   const databaseMaxConnectionsResult = await database.query('SHOW max_connections;')
   const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseName = 'local_db'
-  const databaseOpenedConnectionsResult = await database.query(`SELECT count(*)::int FROM pg_stat_activity WHERE datname = '${databaseName}';` )
-  console.log(databaseOpenedConnectionsResult.rows);
-  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
+  const databaseName = process.env.POSTGRES_DB;
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
