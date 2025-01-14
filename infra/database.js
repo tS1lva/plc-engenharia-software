ead298bcfd5ac71009aac63e19425ef86d4b3232
Tiago Silva <tiago.ps2@puccampinas.edu.br>
2025-01-14 12:34:34 -0300

    handles database ssl connection

--- infra/database.js
+++ infra/database.js
@@ -6,16 +6,11 @@ async function query(queryObject) {
     port: process.env.POSTGRES_PORT,
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
+    password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true
   }
 
-  console.log('Credenciais do Postgres: ', {
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD
-  });
+  console.log('Credenciais do Postgres: ', clientData);
 
   const client = new Client(clientData);
 
