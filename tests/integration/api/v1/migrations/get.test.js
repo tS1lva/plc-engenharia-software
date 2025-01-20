99b6c9b2878e255489edae580b24480f3cbd069d
Tiago Silva <129746635+tS1lva@users.noreply.github.com>
2025-01-20 08:12:08 +0000

    pista lenta `dia 24` - `desafio 1 ok`

--- tests/integration/api/v1/migrations/get.test.js
+++ tests/integration/api/v1/migrations/get.test.js
@@ -1,3 +1,5 @@
+import database from "infra/database.js"
+
 test("GET to /api/v1/migrations should return 200", async () => {
     const response = await fetch ("http://localhost:3000/api/v1/migrations");
     expect(response.status).toBe(200);
@@ -5,5 +7,23 @@ test("GET to /api/v1/migrations should return 200", async () => {
     const responseBody = await response.json();
     console.log(responseBody)
     expect(Array.isArray(responseBody)).toBe(true);
+    expect(responseBody.length).toBeGreaterThan(0);
+
+
+
+    async function query(queryObject) {
+        const clientData = {
+          host: process.env.POSTGRES_HOST,
+          port: process.env.POSTGRES_PORT,
+          user: process.env.POSTGRES_USER,
+          database: process.env.POSTGRES_DB,
+          password: process.env.POSTGRES_PASSWORD
+        }
+    
+        console.log(clientData);
+    }
+
+    const databaseVersionResult = await query('SHOW server_version;')
 
 });
+
